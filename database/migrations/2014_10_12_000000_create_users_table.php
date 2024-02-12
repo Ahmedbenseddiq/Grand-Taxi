43d8671999ef58432db34cbadc4143e0b49293f8
ahmed <benseddiqahmed4@gmail.com>
2024-02-12 14:22:21 +0100

    fixing role problems

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Passager'); // Default role can be 'user' or any other role you prefer
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
